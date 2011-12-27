cbd134dd1fa1797ca185d8d55112d277c54f98a5
Alexandre SKRZYNIARZ <alexandre.skrzyniarz@laposte.net>
2011-12-27 23:58:34 +0000

    autogen.sh now creates empty ChangeLog file

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [ ! -f ChangeLog ]
+then
+    touch ChangeLog
+fi
+
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
 echo "Running libtoolize..." ; libtoolize || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
