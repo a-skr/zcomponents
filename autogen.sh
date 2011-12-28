dee0c8dff4ed1bde9fadff7f4478d3b96366641b
Alexandre SKRZYNIARZ <alexandre.skrzyniarz@laposte.net>
2011-12-28 14:51:44 +0000

    add missing LGPL license headers

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Copyright (c) 2011 Alexandre Skrzyniarz.
+#
+# This file is part of oc2/zcomponents.
+#
+# oc2/zcomponents is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# oc2/zcomponents is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with oc2/zcomponents.  If not, see <http://www.gnu.org/licenses/>.
+
 if [ ! -f ChangeLog ]
 then
     touch ChangeLog
--- tests/test_zreactor_run_halt.cpp
+++ tests/test_zreactor_run_halt.cpp
@@ -1,4 +1,21 @@
-// copyright Alexandre Skrzyniarz 2011-2012, All Rights Reserved.
+/*
+ * Copyright (c) 2011 Alexandre Skrzyniarz.
+ *
+ * This file is part of oc2/zcomponents.
+ *
+ * oc2/zcomponents is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * oc2/zcomponents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with oc2/zcomponents.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <pthread.h>
 #include <oc2/ZReactor.hpp>
